8ba828c93d7eb56cbc520e4067c209cced8dfbee
Diego Marques <diego@progat.com.br>
2015-10-02 16:47:23 -0300

    IO de AFD

--- src/br/inf/ufsc/formais/test/TestRunner.java
+++ src/br/inf/ufsc/formais/test/TestRunner.java
@@ -17,8 +17,8 @@ public class TestRunner {
         DeterminizacaoTeste detT = new DeterminizacaoTeste();
 //        
 //        gT.runTest();
-//        dfaT.runTest();
-        erT.runTest();
+        dfaT.runTest();
+//        erT.runTest();
 //        detT.runTeste();
     }
 }
