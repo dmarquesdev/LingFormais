f6d89aba7f5d9f4425d7ec64126d29166b6abbd4
nathanmolinari <nathanmolinari@gmail.com>
2015-11-04 22:50:25 -0200

    Adicionado exceções nos metódos computar das classes AFD e AFND.

---
+++ src/br/inf/ufsc/formais/exception/EstadosInalcancaveisException.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.inf.ufsc.formais.exception;
+
+import br.inf.ufsc.formais.model.Simbolo;
+import br.inf.ufsc.formais.model.automato.Estados;
+
+/**
+ *
+ * @author Diego
+ */
+public class EstadosInalcancaveisException extends Exception {
+
+    private Estados estados;
+    private Simbolo simbolo;
+
+    public EstadosInalcancaveisException(Estados estado, Simbolo simbolo) {
+        this.estados = estado;
+        this.simbolo = simbolo;
+    }
+
+    public Estados getEstados() {
+        return estados;
+    }
+
+    public Simbolo getSimbolo() {
+        return simbolo;
+    }
+
+}
